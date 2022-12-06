34eeb14a10a4de3e5146dd53ff76824db9de2040
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2022-12-06 16:55:19 -0300

    at

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -5,6 +5,7 @@ export const StyledHeader = styled.header`
     display: flex;
     align-items: center;
     padding: 6px;
+    box-shadow: 0px 0px 10px black;
 
     > div {
         margin-left: 30px;
